7bbc7f770b290175021df6b292c26d2eedfd9109
Anthony Prabowo <anthonyprabowo@users.noreply.github.com>
2021-02-16 11:10:46 -0800

    remove comment and update package.json

--- error.js
+++ error.js
@@ -1,13 +1,15 @@
 // 404 error
 function fourOFour(req, res, next) {
+    console.log("Handling 404 error")
     const err = new Error("File Not Found")
     err.status = 404;
     next(err)
 };
 
 function generalError(err, req, res, next) {
-    if(err.stack) console.error(err)
     if(err.status === 404){
+        console.error(err.message)
+        console.error(err.status)
         res.render('page-not-found', {err})
     } else {
         res.render('error', {err})
